62368ae1167a81afb27e6c1cf3d5f13622a14445
Italo Cedro <italo.cedro.sales@gmail.com>
2019-11-24 01:28:29 -0300

    Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error={false}>
+      Main
+      <small>menor</small>
+    </Title>
+  );
 }
 
 export default Main;
